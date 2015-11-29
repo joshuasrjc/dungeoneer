2783d54b6105b790d6763282ec0e86db81c40c8c
joshuasrjc <joshuasrjc@gmail.com>
2015-11-29 15:44:54 -0800

    Week 2.1.3

--- game/engine/GameEngine.js
+++ game/engine/GameEngine.js
@@ -1,3 +1,5 @@
+var PhysicsBody = require("./PhysicsBody.js");
+
 function Scene()
 {
   var scene = this;
@@ -24,25 +26,72 @@ function Scene()
 
 function GameObject(objData)
 {
-  var gameObject = this;
-  gameObject.scene = null;
+  var self = this;
+  self.components = [];
+  self.name = objData.name;
 
-
-
-  gameObject.start(scene)
+  for(var componentData in objData.components)
   {
-    gameObject.scene = scene;
+    var component = new Component(self);
+    var componentClass = componentClassFromName(componentData.name);
+    componentClass.call(component, componentData);
+    self.components.push(component);
   }
 
-  GameObject.stop()
+  gameObject.getComponent = function(name)
   {
-
+    for(var comp in self.components)
+    {
+      if(comp.name == name)
+      {
+        return comp;
+      }
+    }
+    return null;
   }
+
+  gameObject.start = function(scene)
+  { for(var comp in self.components) { comp.start(scene); } }
+
+  gameObject.update = function(scene)
+  { for(var comp in self.components) { comp.update(scene); } }
+
+  GameObject.stop = function(scene)
+  { for(var comp in self.components) { comp.stop(scene); } }
+}
+
+function Component(gameObject)
+{
+  this.gameObject = gameObject;
+  this.name = "Component";
+  this.start = function(scene){}
+  this.update = function(scene){}
+  this.stop = function(scene){}
+}
+
+function componentClassFromName(name)
+{
+  var componentClass;
+  switch(name)
+  {
+    case "PhysicsBody":
+      componentClass = PhysicsBody;
+      break;
+    default:
+      componentClass = null;
+  }
+
+  return componentClass;
 }
 
 function createScene(physicsEngine)
 {
   return new Scene(physicsEngine);
+}
+
+function createGameObject(objData)
+{
+  return new GameObject(prefabData);
 }
 
 exports.createScene = createScene;
